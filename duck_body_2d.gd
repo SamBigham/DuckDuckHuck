703502ade0fcb749ece21f6c3f8f40480910908b
SamBigham <sambigham3@gmail.com>
2024-04-10 23:47:43 -0500

    Day's work
    added turning around to the duck body

--- duck_body_2d.gd
+++ duck_body_2d.gd
@@ -3,13 +3,17 @@ extends CharacterBody2D
 @onready var Duck = $Sprite2D
 @onready var fris = $Sprite2D2
 @onready var wing = $Sprite2D2/Sprite2D3
+@onready var backfris = $backward
+@onready var backwing = $backward/otherbackwards
+
 const SPEED = 600.0
 const UP_SPEED = -400.0
 var hasDisc = 0
+var originalpos
+var BIGNUMBER = Vector2(-1000, -1000)
 
-#func _ready():
-#	Duck.rotate(70)
-#	fris.rotate(70)
+func _ready():
+	originalpos = fris.position
 	
 func _physics_process(delta):
 		
@@ -21,15 +25,23 @@ func _physics_process(delta):
 		if Duck.global_rotation_degrees > 20:
 			Duck.rotate(-40 * delta)
 			fris.rotate(-40 * delta)
+			backfris.rotate(-40 * delta)
 		else: if Duck.global_rotation_degrees < -20:
 			Duck.rotate(40 * delta)
 			fris.rotate(40 * delta)
+			backfris.rotate(40 * delta)
 		else:
 			Duck.rotation = 0
 			fris.rotation = 0
+			backfris.rotation = 0
+			
 			
 		if velocity.x > 1 || velocity.x < -1 :
 			Duck.animation = "running"
+			if velocity.x > 1:
+				Duck.flip_h = false
+			else: if velocity.x < -1:
+				Duck.flip_h = true
 		else:
 			Duck.animation = "idle"
 		if Input.is_action_pressed("up"):
@@ -48,12 +60,44 @@ func _physics_process(delta):
 		velocity.x = 0
 		velocity.y = 0
 		Duck.rotate(direction / 10)
+		
 		fris.rotate(direction / 10)
+		backfris.rotate(direction / 10)
+		
+		
+
+		if Input.is_action_just_pressed("up") and !Duck.flip_h:
+			if originalpos:
+				fris.position = originalpos
+			Duck.flip_h = true
+			backfris.visible = true
+			originalpos = fris.position
+			fris.position = BIGNUMBER
+			
+#			fris.apply_scale(Vector2(-1,1))
+
+		if Input.is_action_just_pressed("down") and Duck.flip_h:
+			Duck.flip_h = false
+			backfris.visible = false
+			if originalpos:
+				fris.position = originalpos
+			print(originalpos)
+			
 #		wing.rotate(direction / 100)
 	move_and_slide()
 
+
 func disc_visible():
-	fris.visible = true
+	if Duck.flip_h:
+		backfris.visible = true
+		
+		fris.position = BIGNUMBER
+		fris.visible = true
+	else: 
+		if originalpos:
+			fris.position = originalpos
+		fris.visible = true
 	
 func disc_invisible():
 	fris.visible = false
+	backfris.visible = false
--- scenes/fris_path.gd
+++ scenes/fris_path.gd
@@ -3,9 +3,11 @@ extends Area2D
 #@onready var path_follow = $".."
 
 @export var _speed = 1000
-@onready var move = false
+#@onready var move = false
 @onready var path = $"../.."
+@onready var frisbee = $Sprite2D
 @export var floaty = 2
+
 var l = 0
 
 func _ready():
@@ -15,17 +17,21 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 
-	if move:
+	var path_follow = $".."
 
-		var path_follow = $".."
-
-		path_follow.progress += (_speed * delta * (1 - path_follow.get_progress_ratio()
-		 + (path_follow.get_progress_ratio() / floaty ) ) )
+	path_follow.progress += (_speed * delta * (1 - path_follow.get_progress_ratio()
+	 + (path_follow.get_progress_ratio() / floaty ) ) )
+	#when frisbee completes path move is set back to false
+	if path_follow.get_progress_ratio() == 1 or path_follow.get_progress_ratio() == 0:
+		frisbee.animation = "default"
+	else:
+		frisbee.animation = "forward"
 
 func _on_savepoint_timeout():
-	move = true
+	pass
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_released("click"):
-		move = true
+	pass
+#	if event.is_action_released("click"):
+#		move = true
 
